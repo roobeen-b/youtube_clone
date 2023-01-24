22a7ad4087f80c7e112eeb1e32fb4af8a1c291da
Rubin Baidhya <rubinbaidhya@gmail.com>
2023-01-24 13:29:10 +0545

    Minor changes

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,7 +2,7 @@ import { Stack, Box } from "@mui/system";
 import { VideoCard, ChannelCard } from "./";
 
 const Videos = ({ videos, direction }) => {
-  if (!videos.length) return "Loading...";
+  if (!videos?.length) return "Loading...";
   return (
     <Stack
       direction={direction || "row"}
